15304e7e08a62e432204b3d14dc66d6886ea9b96
savio-almeida <142703071+savio-almeida@users.noreply.github.com>
2024-08-26 23:01:26 -0300

    first commit

---
+++ js/firebase/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyATDCR7MJad7A_ZJcau_ayn4ug9gRbBuX0",
+  authDomain: "novoweb-bc1bc.firebaseapp.com",
+  projectId: "novoweb-bc1bc",
+  storageBucket: "novoweb-bc1bc.appspot.com",
+  messagingSenderId: "322324292959",
+  appId: "1:322324292959:web:00807ffe0f4286ee9502ea"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
